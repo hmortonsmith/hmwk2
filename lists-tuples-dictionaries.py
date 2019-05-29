4052fdfce6aeda6cd2f0215cda50b09eb4f67186
Hary Morton-Smith <HMorton-Smith@spartaglobal.com>
2019-05-29 12:21:44 +0100

    added class exercise

---
+++ lists-tuples-dictionaries.py
@@ -0,0 +1,10 @@
+listdef = 'A list is a type of container, which is used to store multiple data at the same time. They are ordered and have a definite count.'
+tupledef = 'Tuples are identical to lists in all respects, except Tuples are immutable.'
+dictionarydef = 'A dictionary is a collection which is unordered, changeable and indexed. In Python dictionaries are written with curly brackets, and they have keys and values.'
+
+print(listdef)
+print('[]')
+print(tupledef)
+print('()')
+print(dictionarydef)
+print('{}')
