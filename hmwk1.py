4d26d38dfc6e72a741328be91080eceb7c3cc232
Hary Morton-Smith <HMorton-Smith@spartaglobal.com>
2019-05-29 09:40:43 +0100

    second commit

---
+++ hmwk1.py
@@ -0,0 +1,36 @@
+#Instance
+instancedef = 'Definition: an example or single occurrence of something. Or to cite (a fact, case, etc.) as an example.'
+
+print(instancedef)
+
+print(type(instancedef))
+
+
+#Variable
+variabledef = 'Variable: memory locations to store values. This means that when you create a variable you reserve some space in memory. Use = to assign/reassign'
+
+print(variabledef)
+
+print(type(variabledef))
+
+varexample = 25
+
+print (varexample)
+
+print(type(varexample))
+
+
+#String
+stringdef = 'A string in Python is a sequence of characters. Strings are immutable. Once defined, they cannot be changed.'
+
+print(stringdef)
+
+print(type(stringdef))
+
+
+#Integer
+intdef = 'An integer is a number data type. Integers are positive or negative whole numbers with no decimal points.'
+
+print(intdef)
+
+print(type(intdef))
