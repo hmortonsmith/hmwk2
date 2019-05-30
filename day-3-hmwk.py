929388a2c78d36e3836110404d3e8e2812a8f960
Hary Morton-Smith <HMorton-Smith@spartaglobal.com>
2019-05-30 22:28:14 +0100

    finished day 3 homework

---
+++ day-3-hmwk.py
@@ -0,0 +1,28 @@
+# What is a class?
+
+print('''A class is a code template for creating objects.\n This defines a set of attributes that will characterize any object that is instantiated from this class. \n\n''')
+
+# What is an instance of a class?
+
+print('An object is an instance of a class.   '
+      'Instance = class(arguments)\n\n')
+
+# What is a method for a class instance
+
+print('''Methods are simply another kind of function that reside in classes. \nYou create and work with methods in Python in precisely the same way that you do functions,\n except that methods are always associated with a class. You can create two kinds of methods: those associated with the class itself and those associated with an instance of a class. \nIt’s important to differentiate between the two.\n\n''')
+
+
+# 4 pillars of OOP
+
+# Abstraction
+print('''Abstraction is selecting data from a larger pool to show only the relevant details to the object.\n It helps to reduce programming complexity and effort. \n\n''')
+
+# Encapsulation
+print('Encapsulation describes the idea of bundling data and methods that work on that data within one unit\n\n')
+
+# Inheritance
+print('Inheritance is when a class uses code constructed within another class.\n\n')
+
+# Polymorphism
+print('Polymorphism describes the concept that objects of different types can be accessed through the same interface.')
+
